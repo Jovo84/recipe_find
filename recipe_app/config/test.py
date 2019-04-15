909825e06ae36289194689b99cf79fd731ddfeeb
Jovo Dragicevic <jovodragicevic84@gmail.com>
2019-04-15 12:22:55 +0200

    gitignore change

--- recipe_app/config/test.py
+++
@@ -1,7 +0,0 @@
-config = {
-    "api_base_url": "http://localhost:5000/api/",
-    "mysql_host": "localhost",
-    "mysql_username": "root",
-    "mysql_password": "root",
-    "mysql_db_name": "lottalotto"
-}
